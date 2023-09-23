bd96c45aed06fe168955a0241a1b4522edd1fb2c
lipeng <1162423147@qq.com>
2023-09-23 22:29:33 +0800

    [new] 新建

---
+++ lib/phoenix_sugsearch.dart
@@ -0,0 +1,12 @@
+/*
+ * @Author: lipeng 1162423147@qq.com
+ * @Date: 2023-09-23 22:20:09
+ * @LastEditors: lipeng 1162423147@qq.com
+ * @LastEditTime: 2023-09-23 22:28:00
+ * @FilePath: /phoenix_sugsearch/lib/phoenix_sugsearch.dart
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+library phoenix_sugsearch;
+
+///
+export 'ios_search_text.dart';
